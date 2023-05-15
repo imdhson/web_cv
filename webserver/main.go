b6406a0139b68f1aeda591af3f1330ca47988c8d
imdhson <hhammer1234@gmail.com>
2023-05-15 21:43:33 +0900

    출력 문구 수정

--- webserver/main.go
+++ webserver/main.go
@@ -97,7 +97,7 @@ func resultHanlder(w http.ResponseWriter, r *http.Request) error {
 	willfilePath := "../files/" + willfileName
 	file, err := ioutil.ReadFile(willfilePath)
 	if err != nil {
-		fmt.Println("www/main.html 을 로드할 수 없음")
+		fmt.Println(willfilePath, " 로드할 수 없음")
 		//log.Fatal(err)
 	} else {
 		w.Header().Set("Content-Type", "image/jpeg; charset=utf-8") // 이미지인 경우
