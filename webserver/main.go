d514b5c824e7e3352e40631fe80a0d0593930c14
imdhson <hhammer1234@gmail.com>
2023-05-15 11:55:01 +0900

    added const PORT

--- webserver/main.go
+++ webserver/main.go
@@ -56,7 +56,7 @@ func resultHanlder(w http.ResponseWriter, r *http.Request) error {
 
 func dotFileType(in string) string { //파일 이름을 받으면 . 이후의 확장자만 리턴하여 줍니다.
 	in2 := []rune(in)
-	for i := len(in2) - 1; i >= 0; i-- {
+	for i := len(in2) - 1; i >= 0; i-- { //파일 중간에 . 이 들어가는 경우가 있어서 뒤부터 순회
 		v := string(in2[i])
 		if v == "." {
 			return string(in2[i+1:])
@@ -117,9 +117,10 @@ func urlHandle(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	const PORT int = 8080
 	server := http.NewServeMux()
 	server.Handle("/", http.HandlerFunc(urlHandle))
-	err := http.ListenAndServe(":8080", server)
+	err := http.ListenAndServe(":"+strconv.Itoa(PORT), server)
 	if err != nil { // http 서버 시작 중 문제 발생시
 		log.Fatal(err)
 		panic(err)
