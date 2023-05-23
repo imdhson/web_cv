47e64a93a40f281047f9276f48624fbe6e52b449
imdhson <hhammer1234@gmail.com>
2023-05-23 13:41:25 +0900

    yolo 웹에서 정상 작동화

--- webserver/webcvpkg/Exec_cv.go
+++ webserver/webcvpkg/Exec_cv.go
@@ -16,8 +16,8 @@ func Exec_cv(mode string, filename string) bool {
 	arg1 := "../opencv/" + vexec
 	arg2 := "../files/" + filename
 	//arg3 := dotFileType(filename)
-	fmt.Println("실행중: python3", arg1, arg2)
-	cmd := exec.Command("python3", arg1, arg2) // 예시: python ../opencv/main.py ive.jpeg 를 터미널에서 실행하는 것과 같은 효과임
+	fmt.Println("실행중: python", arg1, arg2)
+	cmd := exec.Command("python", arg1, arg2) // 예시: python ../opencv/main.py ive.jpeg 를 터미널에서 실행하는 것과 같은 효과임
 	cmd.Stdout = os.Stdout
 
 	if err := cmd.Run(); err != nil {
