636dcabd538fcfad8e8bc4330204852698bab367
lastmonth00 <rmsidzhkd@naver.com>
2023-05-23 13:31:38 +0900

    yolo.py

--- opencv/yolo.py
+++ opencv/yolo.py
@@ -11,9 +11,8 @@ model_name = "yolov3" # 사물 인식 모델 이름
 
 result = cv.detect_common_objects(img, confidence=conf, model=model_name)
 
-output_path = path
 result_img = cv.object_detection.draw_bbox(img, *result) # result 결과를 이미지에 반영
-cv2.imwrite(out_path, result_img) # 반영된 이미지로 파일 대체
+cv2.imwrite(path, result_img) # 반영된 이미지로 파일 대체
 
 
 
