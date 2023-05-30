35c28af7705b890e5a938e452f6c48d9652c85dc
lastmonth00 <rmsidzhkd@naver.com>
2023-05-30 20:53:36 +0900

    canny 추가

---
+++ opencv/canny.py
@@ -0,0 +1,12 @@
+import cv2
+import sys
+
+path = sys.argv[1]
+img = cv2.imread(path)
+
+gray_img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+threshold1 = 0
+threshold2 = 360
+edge_img = cv2.Canny(gray_img, threshold1, threshold2)
+cv2.imwrite(path, edge_img)
